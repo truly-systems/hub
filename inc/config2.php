b720cdd09821c266621cef481e6f41a78831af0e
Dev2_Luan <luan@suldopara.com.br>
2016-10-10 15:46:00 -0300

    Ajuste instalador include

---
+++ inc/config2.php
@@ -0,0 +1,9 @@
+<?php
+			$dados_api = array(
+					"host" => "http://demo.trulysystems.com/glpi/apirest.php",
+					"app_token" => "ea7hdxpex2agunikllma6clophkqyoicyoubmkqjm" 
+
+				);
+
+			?>
+			
